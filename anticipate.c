e6c48351cf158d93e905fbaabda660ab09857161
Miles Davis <davis.g.miles@gmail.com>
2018-04-01 13:46:07 +0930

    Ncurses setup.

--- anticipate.c
+++ anticipate.c
@@ -9,8 +9,14 @@
 typedef struct tm tm_t;
 
 const int MAX_CHARS = 256;
+const int COLOUR_WINDOW_ID = 1;
+const int COLOUR_SHADOW_ID = 2;
+const int WINDOW_WIDTH = 55;
+const int WINDOW_HEIGHT = 9;
 
 void usage(void);
+void setupNcurses(void);
+WINDOW* createWindow(void);
 
 int main(int argc, char* argv[])
 {
@@ -32,6 +38,12 @@ int main(int argc, char* argv[])
 
 	targetTimeSec = mktime(&targetTime);
 
+	setupNcurses();
+	WINDOW* window = createWindow();
+
+	delwin(window);
+	endwin();
+
 	return 0;
 }
 
@@ -42,4 +54,37 @@ void usage(void)
 		"                 (time is in 24-hour format)\n"
 		"       TEXT      What's the thing you're anticipating?\n"
 		"                 (one string)\n");
+}
+
+void setupNcurses(void)
+{
+	initscr();
+	start_color();
+	use_default_colors();
+	curs_set(0);
+	cbreak();
+	keypad(stdscr, true);
+	noecho();
+	nodelay(stdscr, true);
+	refresh();
+
+	init_pair(COLOUR_WINDOW_ID, COLOR_WHITE, COLOR_RED);
+	init_pair(COLOUR_SHADOW_ID, COLOR_BLACK, COLOR_BLACK);
+}
+
+WINDOW* createWindow(void)
+{
+	int termWidth;
+	int termHeight;
+	getmaxyx(stdscr, termHeight, termWidth);
+
+	int windowX = termWidth / 2 - (WINDOW_WIDTH / 2);
+	int windowY = termHeight / 2 - (WINDOW_HEIGHT / 2);
+
+	WINDOW* window = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, windowY, windowX);
+	box(window, 0, 0);
+	wbkgd(window, COLOR_PAIR(COLOUR_WINDOW_ID));
+	wrefresh(window);
+
+	return window;
 }
